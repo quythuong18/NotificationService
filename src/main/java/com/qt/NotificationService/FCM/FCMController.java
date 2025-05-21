18dce88c31ca7c67fba8c176c312d5d1127b7d4b
quythuong <quythuong0108@gmail.com>
2025-05-21 07:28:14 +0700

    update FCM controller

--- src/main/java/com/qt/NotificationService/FCM/FCMController.java
+++ src/main/java/com/qt/NotificationService/FCM/FCMController.java
@@ -1,26 +1,44 @@
 package com.qt.NotificationService.FCM;
 
+import com.qt.NotificationService.security.JWTService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("/api/v1/notification")
 @RequiredArgsConstructor
 public class FCMController {
     private final FCMService fcmService;
+    private final JWTService jwtService;
 
-    @PostMapping("/register-token")
-    public ResponseEntity<UserFCMToken> registerToken(@RequestBody UserFCMToken userFCMToken) {
+    @PostMapping("/token")
+    public ResponseEntity<?> registerToken(
+            @RequestHeader("Authorization") String bearerToken,
+            @RequestBody UserFCMToken userFCMToken) {
         if(userFCMToken == null)
             throw new IllegalArgumentException("Body is null");
+        if(!extractAndValidateBearerToken(bearerToken, userFCMToken.getUsername()))
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                                 .body("Authentication failed: Invalid token");
         return ResponseEntity.ok(fcmService.registerFCMToken(userFCMToken));
     }
-    @PostMapping("/update-token")
-    public ResponseEntity<UserFCMToken> updateToken(@RequestBody UserFCMToken userFCMToken) {
+    @PutMapping("/token")
+    public ResponseEntity<?> updateToken(
+            @RequestHeader("Authorization") String bearerToken,
+            @RequestBody UserFCMToken userFCMToken
+    ) {
         if(userFCMToken == null)
             throw new IllegalArgumentException("Body is null");
+        if(!extractAndValidateBearerToken(bearerToken, userFCMToken.getUsername()))
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Authentication failed: Invalid token");
         return ResponseEntity.ok(fcmService.updateFCMToken(userFCMToken));
+    }
+
+    public Boolean extractAndValidateBearerToken(String bearerToken, String username) {
+        if(bearerToken == null || !bearerToken.startsWith("Bearer ")) return Boolean.FALSE;
+        String token = bearerToken.substring(7);
+        return jwtService.isValid(token, username);
     }
 }
